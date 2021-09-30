47cb963a9710dfc18ab33ddf60a5754b7cddd2f0
Ronaldrenzo <>
2021-09-30 00:08:57 +0000

    RR

--- Estcondicional/Regalos.java
+++ Estcondicional/Regalos.java
@@ -6,16 +6,28 @@ public class Regalos{
 static Scanner sc = new Scanner(System.in);
 
 static void algoritmoregalos(){
-  //
-
-  //
-
-  //
-
-  //
+  //Definir variable
+  double monto=0;
+  String regalo="";
+  //Datos de entrada
+  System.out.println("Ingrese el monto que dispone:");
+  monto=sc.nextDouble();
+  //Proceso
+  if(monto<=10){
+    regalo+="\nTarjeta";
+  } else if(monto>=11 && monto<=100){
+    regalo="\nChocolate\nTarjeta";
+  }else if(monto>=101 && monto<=250){
+    regalo="\nFlores\nChocolate\nTarjeta";
+  }else{
+    regalo="\nAnillo\nFlores\nChocolate\nTarjeta";
+  }
+  //Datos de salida
+  System.out.println("Ud. puede elegir los siguientes:"+ regalo);
 }
-public static void main(String[] arg){
 
+public static void main(String[] arg){
+algoritmoregalos();
 }
   
 }
